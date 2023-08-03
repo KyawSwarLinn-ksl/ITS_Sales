5b9517db616c41b6b24e8bb8d4c1607a53a322ae
KyawSwarLinn-ksl <kyawswarlinn@gicjp.com>
2023-08-03 15:21:47 +0900

    Folder_structure

---
+++ resources/js/EngineerInfo/InsertEngineerInfo.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import * as All from "@coreui/react";
+
+
+class InsertEngineerInfo extends Component {
+    state = {  }
+    render() {
+        return "hello";
+    }
+}
+
+export default InsertEngineerInfo ;
