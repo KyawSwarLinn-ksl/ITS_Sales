17169d7111582e5ef4aa317f21163994953a613f
Kyawswarlinn@GIC <kyawswarlinn@gicjp.com>
2023-08-04 15:53:27 +0900

    Navbar_作成

--- resources/js/router/ItsRouter.jsx
+++ resources/js/router/ItsRouter.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ITSNavbar from "../components/ITSNavbar";
 
 class ItsRouter extends React.Component {
     render() {
         return (
             <React.Fragment>
-               <div>ddddddddddddddddddvvvvvvvvvv</div>
+               <ITSNavbar/>
             </React.Fragment>
         );
     }
