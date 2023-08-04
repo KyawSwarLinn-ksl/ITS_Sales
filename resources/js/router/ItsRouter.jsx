984d20b01c9b8e0865e0247f844f991c85cbb193
Zin-Mon-Thet <zinmonthet@gicjp.com>
2023-08-04 12:00:09 +0900

    環境エラー修正

---
+++ resources/js/router/ItsRouter.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class ItsRouter extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+               <div>ddddddddddddddddddvvvvvvvvvv</div>
+            </React.Fragment>
+        );
+    }
+}
+export default ItsRouter;
+ReactDOM.render(<ItsRouter />, document.getElementById("app"));
