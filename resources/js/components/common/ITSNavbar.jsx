62d5f57a12393cc5649093c0ad1a3f88e86a8efe
Zin-Mon-Thet <zinmonthet@gicjp.com>
2023-08-07 13:57:15 +0900

    フォームデザイン

--- resources/js/components/common/ITSNavbar.jsx
+++ resources/js/components/common/ITSNavbar.jsx
@@ -5,7 +5,8 @@ import { Nav,Navbar,NavDropdown } from "react-bootstrap";
 import imgs from "/images/gic_logo.png";
 import { BsFillHouseFill } from "react-icons/bs";
 import "../../../css/common.css";
-import { Link } from "react-router-dom";
+import properties from "../../properties";
+
 function ITSNavbar() {
     useEffect(() => {
 
@@ -29,8 +30,8 @@ function ITSNavbar() {
     </Navbar.Brand>
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto">
-          <Navbar.Brand href="/" className="brand_title">
-              グローバルイノベーションコンサルティング株式会社
+          <Navbar.Brand href="/" className="brand-title">
+            {properties.lbl_companyName}
           </Navbar.Brand>
         </Nav>
       </Navbar.Collapse>
@@ -41,29 +42,29 @@ function ITSNavbar() {
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav>
-          <span className="home-icon"><BsFillHouseFill /></span><Nav.Link  href="/home">ホーム</Nav.Link>
-          <NavDropdown title="技術者管理" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="/engineerList" className="dropdown_item">技術者情報一覧</NavDropdown.Item>
-            <NavDropdown.Item href="/insertEngineer" className="dropdown_item">技術者情報登録</NavDropdown.Item>
-            <NavDropdown.Item href="/searchEngineer" className="dropdown_item">技術者一覧詳細検索</NavDropdown.Item>
-            <NavDropdown.Item href="/engineerSearchResult" className="dropdown_item">技術者一覧検索結果表示設定</NavDropdown.Item>            
+          <span className="home-icon"><BsFillHouseFill /></span><Nav.Link  href="/home">{properties.lbl_home}</Nav.Link>
+          <NavDropdown title={properties.lbl_engineerTitle} id="collasible-nav-dropdown">
+            <NavDropdown.Item href="/engineerList" className="dropdown_item">{properties.lbl_engineerListTitle}</NavDropdown.Item>
+            <NavDropdown.Item href="/insertEngineer" className="dropdown_item">{properties.lbl_insertEngineerTitle}</NavDropdown.Item>
+            <NavDropdown.Item href="/searchEngineer" className="dropdown_item">{properties.lbl_searchEngineerTitle}</NavDropdown.Item>
+            <NavDropdown.Item href="/engineerSearchResult" className="dropdown_item">{properties.lbl_engineerSearchResultTitle}</NavDropdown.Item>            
           </NavDropdown>
-          <NavDropdown title="受・発注管理" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="/orderList" className="dropdown_item">受・発注情報一覧</NavDropdown.Item>
-            <NavDropdown.Item href="/insertOrder" className="dropdown_item">受・発注情報登録</NavDropdown.Item>
-            <NavDropdown.Item href="/searchOrder" className="dropdown_item">受・発注情報一覧詳細検索</NavDropdown.Item>
-            <NavDropdown.Item href="/orderSearchResult" className="dropdown_item">受・発注情報一覧検索結果表示設定</NavDropdown.Item>
+          <NavDropdown title={properties.lbl_orderTitle} id="collasible-nav-dropdown">
+            <NavDropdown.Item href="/orderList" className="dropdown_item">{properties.lbl_orderListTitle}</NavDropdown.Item>
+            <NavDropdown.Item href="/insertOrder" className="dropdown_item">{properties.lbl_insertOrderTitle}</NavDropdown.Item>
+            <NavDropdown.Item href="/searchOrder" className="dropdown_item">{properties.lbl_searchOrderTitle}</NavDropdown.Item>
+            <NavDropdown.Item href="/orderSearchResult" className="dropdown_item">{properties.lbl_orderSearchResultTitle}</NavDropdown.Item>
           </NavDropdown>
-          <NavDropdown title="Offshore請負P(仕掛状況)" className="drop_down" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="/sales" className="dropdown_item">売上</NavDropdown.Item>
-            <NavDropdown.Item href="/insertProgressStatus" className="dropdown_item">仕掛状況登録</NavDropdown.Item>
+          <NavDropdown title={properties.lbl_offshoreTitle} className="drop_down" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="/sales" className="dropdown_item">{properties.lbl_salesTitle}</NavDropdown.Item>
+            <NavDropdown.Item href="/insertProgressStatus" className="dropdown_item">{properties.lbl_insertProgressStatusTitle}</NavDropdown.Item>
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
       <Nav>
         <NavDropdown title={UserMenu} id="collasible-nav-dropdown" className="user-image">
-            <NavDropdown.Item href="#action/3.1" className="dropdown-item">パスワード変更</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2" className="dropdown-item">ログアウト</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1" className="dropdown-item">{properties.lbl_passwordUpdate}</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2" className="dropdown-item">{properties.lbl_logout}</NavDropdown.Item>
         </NavDropdown>
         <Nav.Link href="#deets" className="login-user-name">チョー様</Nav.Link>
       </Nav>
--- resources/js/components/order/InsertOrder.jsx
+++ resources/js/components/order/InsertOrder.jsx
@@ -1,12 +1,113 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import ReactDOM from "react-dom";
+import * as CuiForm from "@coreui/react";
 
 class InsertOrder extends React.Component {
     render() {
         return (
-            <div>
-              Insert Order Page!!!!!!!!!
-            </div>
+            <Container>
+                <CuiForm.CForm>
+                    <div className="row mb-2 mt-3">
+                        <div className="col-lg-6 col-md-6">
+                            <div className="row">
+                                <div className="col-lg-4 col-md-4">
+                                    <CuiForm.CFormLabel>発注_請求金額（CT税抜）</CuiForm.CFormLabel>
+                                </div>
+                                <div className="col-lg-6 col-md-8">
+                                    <CuiForm.CFormInput type="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-md-6">
+                            <div className="row">
+                                <div className="col-lg-4 col-md-4">
+                                    <CuiForm.CFormLabel>Email address</CuiForm.CFormLabel>
+                                </div>
+                                <div className="col-lg-6 col-md-8">
+                                    <CuiForm.CFormInput type="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row mb-2">
+                        <div className="col-lg-6 col-md-6">
+                            <div className="row">
+                                <div className="col-lg-4 col-md-4">
+                                    <CuiForm.CFormLabel>Email address</CuiForm.CFormLabel>
+                                </div>
+                                <div className="col-lg-6 col-md-8">
+                                    <CuiForm.CFormInput type="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-md-6">
+                            <div className="row">
+                                <div className="col-lg-4 col-md-4">
+                                    <CuiForm.CFormLabel>Email address</CuiForm.CFormLabel>
+                                </div>
+                                <div className="col-lg-6 col-md-8">
+                                    <CuiForm.CFormInput type="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row mb-2">
+                        <div className="col-lg-6 col-md-6">
+                            <div className="row">
+                                <div className="col-lg-4 col-md-4">
+                                    <CuiForm.CFormLabel>Email address</CuiForm.CFormLabel>
+                                </div>
+                                <div className="col-lg-6 col-md-8">
+                                    <CuiForm.CFormInput type="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-md-6">
+                            <div className="row">
+                                <div className="col-lg-4 col-md-4">
+                                    <CuiForm.CFormLabel>Email address</CuiForm.CFormLabel>
+                                </div>
+                                <div className="col-lg-6 col-md-8">
+                                    <CuiForm.CFormInput type="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row mb-2">
+                        <div className="col-lg-6 col-md-6">
+                            <div className="row">
+                                <div className="col-lg-4 col-md-4">
+                                    <CuiForm.CFormLabel>Email address</CuiForm.CFormLabel>
+                                </div>
+                                <div className="col-lg-6 col-md-8">
+                                    <CuiForm.CFormInput type="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-md-6">
+                            <div className="row">
+                                <div className="col-lg-4 col-md-4">
+                                    <CuiForm.CFormLabel>Email address</CuiForm.CFormLabel>
+                                </div>
+                                <div className="col-lg-6 col-md-8">
+                                    <CuiForm.CFormInput type="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <CuiForm.CFormCheck
+                        className="mb-3"
+                        label="Check me out"
+                        onChange={(e) => {
+                        console.log(e.target)
+                        }}
+                    />
+                    <CuiForm.CButton type="submit" color="primary">
+                        Submit
+                    </CuiForm.CButton>
+                </CuiForm.CForm>
+            </Container>
         );
     }
 }
