64b538a074859221a81dc867a9965c5e2525b9ac
Zin-Mon-Thet <zinmonthet@gicjp.com>
2023-08-07 14:26:49 +0900

    デザイン

--- resources/js/components/order/InsertOrder.jsx
+++ resources/js/components/order/InsertOrder.jsx
@@ -1,112 +1,76 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import ReactDOM from "react-dom";
-import * as CuiForm from "@coreui/react";
+import * as bForm from 'react-bootstrap';
 
 class InsertOrder extends React.Component {
     render() {
         return (
             <Container>
-                <CuiForm.CForm>
-                    <div className="row mb-2 mt-3">
-                        <div className="col-lg-6 col-md-6">
+                <bForm.Form>
+                    <bForm.Form.Group className="row mb-3 mt-3" controlId="formBasicEmail">
+                        <div className="col-lg-6 col-md-6 mb-2">
                             <div className="row">
                                 <div className="col-lg-4 col-md-4">
-                                    <CuiForm.CFormLabel>発注_請求金額（CT税抜）</CuiForm.CFormLabel>
+                                    <bForm.Form.Label>Email address</bForm.Form.Label>
                                 </div>
                                 <div className="col-lg-6 col-md-8">
-                                    <CuiForm.CFormInput type="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                    <bForm.Form.Control type="email" placeholder="Enter email" />
                                 </div>
                             </div>
                         </div>
-                        <div className="col-lg-6 col-md-6">
+                        <div className="col-lg-6 col-md-6 mb-2">
                             <div className="row">
                                 <div className="col-lg-4 col-md-4">
-                                    <CuiForm.CFormLabel>Email address</CuiForm.CFormLabel>
+                                    <bForm.Form.Label>Email address</bForm.Form.Label>
                                 </div>
                                 <div className="col-lg-6 col-md-8">
-                                    <CuiForm.CFormInput type="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                    <bForm.Form.Control type="email" placeholder="Enter email" />
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="row mb-2">
-                        <div className="col-lg-6 col-md-6">
+                        <div className="col-lg-6 col-md-6 mb-2">
                             <div className="row">
                                 <div className="col-lg-4 col-md-4">
-                                    <CuiForm.CFormLabel>Email address</CuiForm.CFormLabel>
+                                    <bForm.Form.Label>Email address</bForm.Form.Label>
                                 </div>
                                 <div className="col-lg-6 col-md-8">
-                                    <CuiForm.CFormInput type="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                    <bForm.Form.Control type="email" placeholder="Enter email" />
                                 </div>
                             </div>
                         </div>
-                        <div className="col-lg-6 col-md-6">
+                        <div className="col-lg-6 col-md-6 mb-2">
                             <div className="row">
                                 <div className="col-lg-4 col-md-4">
-                                    <CuiForm.CFormLabel>Email address</CuiForm.CFormLabel>
+                                    <bForm.Form.Label>Email address</bForm.Form.Label>
                                 </div>
                                 <div className="col-lg-6 col-md-8">
-                                    <CuiForm.CFormInput type="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                    <bForm.Form.Control type="email" placeholder="Enter email" />
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="row mb-2">
-                        <div className="col-lg-6 col-md-6">
+                        <div className="col-lg-6 col-md-6 mb-2">
                             <div className="row">
                                 <div className="col-lg-4 col-md-4">
-                                    <CuiForm.CFormLabel>Email address</CuiForm.CFormLabel>
+                                    <bForm.Form.Label>Email address</bForm.Form.Label>
                                 </div>
                                 <div className="col-lg-6 col-md-8">
-                                    <CuiForm.CFormInput type="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                    <bForm.Form.Control type="email" placeholder="Enter email" />
                                 </div>
                             </div>
                         </div>
-                        <div className="col-lg-6 col-md-6">
+                        <div className="col-lg-6 col-md-6 mb-2">
                             <div className="row">
                                 <div className="col-lg-4 col-md-4">
-                                    <CuiForm.CFormLabel>Email address</CuiForm.CFormLabel>
+                                    <bForm.Form.Label>Email address</bForm.Form.Label>
                                 </div>
                                 <div className="col-lg-6 col-md-8">
-                                    <CuiForm.CFormInput type="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                    <bForm.Form.Control type="email" placeholder="Enter email" />
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="row mb-2">
-                        <div className="col-lg-6 col-md-6">
-                            <div className="row">
-                                <div className="col-lg-4 col-md-4">
-                                    <CuiForm.CFormLabel>Email address</CuiForm.CFormLabel>
-                                </div>
-                                <div className="col-lg-6 col-md-8">
-                                    <CuiForm.CFormInput type="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 col-md-6">
-                            <div className="row">
-                                <div className="col-lg-4 col-md-4">
-                                    <CuiForm.CFormLabel>Email address</CuiForm.CFormLabel>
-                                </div>
-                                <div className="col-lg-6 col-md-8">
-                                    <CuiForm.CFormInput type="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <CuiForm.CFormCheck
-                        className="mb-3"
-                        label="Check me out"
-                        onChange={(e) => {
-                        console.log(e.target)
-                        }}
-                    />
-                    <CuiForm.CButton type="submit" color="primary">
-                        Submit
-                    </CuiForm.CButton>
-                </CuiForm.CForm>
+                    </bForm.Form.Group>
+                </bForm.Form>
             </Container>
         );
     }
